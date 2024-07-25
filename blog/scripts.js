adba6157eeb9b209d99147bb95ba9947cab7efe1
Shawshank <144000256+shawshank725@users.noreply.github.com>
2024-07-25 20:31:10 +0530

    Delete blog directory

--- blog/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('copy-button').addEventListener('click', function() {
-    const code = document.getElementById('python-code').innerText;
-    navigator.clipboard.writeText(code).then(function() {
-        alert('Code copied to clipboard!');
-    }, function(err) {
-        console.error('Could not copy text: ', err);
-    });
-});
