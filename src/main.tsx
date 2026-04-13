4c00aef013127bbe6d54f11b1aba1e01fe1b198d
Shashank Verma <vermashashank070205@gmail.com>
2026-04-13 10:08:01 +0530

    changed ts to tsx and added path alias for context folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/App.tsx'
+import { DarkModeProvider } from '@/context/context'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
   </StrictMode>,
 )
