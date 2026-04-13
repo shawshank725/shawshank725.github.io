d3e5edea501861874c045d9d5a1f787c7e6b9006
Shashank Verma <vermashashank070205@gmail.com>
2026-04-13 15:14:25 +0530

    fixed the home page container styling

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,15 +1,15 @@
 import '@styles/HomePage.css';
-import {ReactTyped } from 'react-typed';
+import { ReactTyped } from 'react-typed';
 import type { ReferenceProp } from '@/types/ReferenceProp';
 import profilePhoto from '@assets/Profile Photo.jpeg';
 
-export default function HomePage({reference}: ReferenceProp) {
+export default function HomePage({ reference }: ReferenceProp) {
     return (
         <div className="homePageContainer" ref={reference}>
             <div className='introductionProfilePhotoContainer'>
                 <div className='profilePhotoTypewriterContainer'>
-                    <img src={profilePhoto} className='profilePhoto'/>
-                    <ReactTyped 
+                    <img src={profilePhoto} className='profilePhoto' />
+                    <ReactTyped
                         strings={[
                             "Hi there!", "I'm Shashank"
                         ]}
@@ -20,11 +20,14 @@ export default function HomePage({reference}: ReferenceProp) {
                     />
                 </div>
                 <div className='aboutMeFocusContainer'>
-                    <span className='introductionText'>
-                        I'm a Full stack Java Developer with expertise in building backend systems using Spring Boot. 
-                        I have published my own Java package (CLI tool) to Maven Central.
-                        My interests include ethical hacking and game development as well.
-                    </span>
+                    <div className='introductionTextContainer'>
+                        <span className='introductionText'>Hi! I'm Shashank.</span>
+                        <span className='introductionText'>I'm a Full stack Java Developer with expertise in building backend systems using Spring Boot.</span>
+                        <span className='introductionText'>I use Linux BTW. Currently I'm on Linux Mint.</span>
+                        <span className='introductionText'>I have published my own Java package (CLI tool) to Maven Central.</span>
+                        <span className='introductionText'>My interests include ethical hacking and game development as well.</span>
+                    </div>
+
                     <div className='focusContainer'>
                         <div className='focusNamePercentage'>
                             Frontend (30%)
